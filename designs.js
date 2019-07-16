3bc0fd23ed1200618927905e143be1161a0254b5
agluminat <sagmeister@gmail.com>
2019-07-15 20:42:01 -0400

    type: feat
    Implementing JavaScript code for Pixel Art Maker.

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
-// Select color input
-// Select size input
+// Defining variables by selecting the DOM elements.
+const color = document.getElementById('colorPicker');
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
+const form = document.getElementById('sizePicker');
+const table = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+// Handle user's submit, with maximum value 40.
+form.addEventListener('submit', function(event) {
+    table.innerHTML = '';
+    event.preventDefault();
+    if (height.value <= 40 && width.value <= 40) {
+        makeGrid();
+    } else {
+        alert('Maximum size is 40 x 40.');
+    }
+});
 
+// When user clicks on a cell it changes its background color.
+table.addEventListener('click', function(event) {
+    if (event.target.tagName === 'TD') {
+        event.target.style.backgroundColor = color.value;
+    }
+});
+
+// Create the canvas with rows(x) and columns(y).
 function makeGrid() {
-
-// Your code goes here!
-
+    for (let x = 1; x <= height.value; x++) {
+        var row = document.createElement('tr');
+        for (let y = 1; y <= width.value; y++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
 }
